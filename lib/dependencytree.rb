ada41070da1775b28a1c3d2f1349602a3abb1754
Stephan Fuhrmann <stephan.fuhrmann@1und1.de>
2018-03-28 14:50:08 +0200

    dependencytree is now traversing directory trees dependencyaggregator has very much more support for proper class models now

--- lib/dependencytree.rb
+++ lib/dependencytree.rb
@@ -9,23 +9,43 @@ require 'json'
 
 module Dependencytree
 
+  def self.handle_path(options, consumer, path)
+puts path
+    if options[:ignore].match(path)
+      return
+    end
+    if File.directory?(path)
+      Dir.entries(path).each { |x|
+        resolved = File.join(path, x)
+        handle_path(options, consumer, resolved) if File.directory?(resolved) && x != "." && x != ".."
+        handle_path(options, consumer, resolved) if File.file?(resolved) && options[:pattern].match(resolved)
+      }
+    elsif File.file?(path)
+      tree = Parser::CurrentRuby.parse_file(path)
+      consumer.visit(path, tree)
+    end
+  end
+
   options = {}
+  options[:ignore] = Regexp.new("^$")
+  options[:pattern] = Regexp.new(".*\.rb")
   OptionParser.new do |opt|
     opt.on("-v", "--verbose", "Verbose output") { |o| options[:verbose] = true }
+    opt.on("-p", "--pattern", "Pattern to accept source codes with (.*\.rb)") { |o| options[:pattern] = Regexp.new(o) }
+    opt.on("-i", "--ignore", "Paths to not load (^$)") { |o| options[:ignore] = Regexp.new(o) }
     opt.on_tail("-h", "--help", "Show this message") do
       puts opt
       exit
     end
   end.parse!
 
+  consumer = DependencyAggregator.new()
   ARGV.each { |path|
-    puts "path" if options[:verbose]
-    tree = Parser::CurrentRuby.parse_file(path)
-#    printer = ASTPrinter.new(path, tree)
-    printer = DependencyAggregator.new(path, tree)
-    printer.visit
-    puts printer.to_json
+    puts "Path #{path}" if options[:verbose]
+
+    handle_path(options, consumer, path)
   }
+  puts consumer.to_json
 
 end
 
--- lib/dependencytree/dependencyaggregator.rb
+++ lib/dependencytree/dependencyaggregator.rb
@@ -1,17 +1,29 @@
 require "dependencytree/version"
 require 'parser/current'
+require "dependencytree/classmodel"
 
 module Dependencytree
   class DependencyAggregator
-    def initialize(path, tree)
-      @path = path
-      @tree = tree
-      @references = {}
-      @current_class = :anonymous
+    def initialize()
+      @path = nil
+      @current_module_name = :anonymous
+      @current_class_name = :anonymous
+      @classes = []
     end
 
     def to_json
-      @references.to_json
+      @classes.to_json
+    end
+
+    def flatten_const_tree(node)
+      raise ArgumentError, "type needs to be const (#{node.type})" if node.type != :const
+      raise ArgumentError, "Children count needs to be 2 (#{node.children.length})" if node.children.length != 2
+
+      result = node.children[1..1]
+      if node.children[0] && node.children[0].type == :const
+        result = flatten_const_tree(node.children[0]) + result
+      end
+      result
     end
 
     def const(node)
@@ -22,26 +34,53 @@ module Dependencytree
       raise ArgumentError, "Children count needs to be 2 (#{node.children.length})" if node.children.length != 2
 
       if @current_class
-        reference = node.children[1]
-        @references[@current_class] = [] if ! @references[@current_class]
-        @references[@current_class] << reference.to_sym
+        reference = flatten_const_tree(node)
+        @current_class.add_reference(reference)
         puts "added #{reference}"
       end
     end
 
-    def clazz(node)
-      puts "clazz"
-      
-      raise ArgumentError, "Children count for class is != 2 (#{node.children.length})" if node.children.length != 3
+    def _module(node)
+      raise ArgumentError, "Children count for module is != 2 (#{node.children.length})" if node.children.length != 2
+      raise ArgumentError, "First module child needs to be a const (#{node.children[0].type} #{node.children[0].type})" if node.children[0].type != :const
+
+      puts "module #{node.children[0].children[1]}"
+
+      old_module = @current_class
+      @current_module_name = node.children[0].children[1]
+      # recurse over the contents of the module
+      visit_children(node.children[1..node.children.length])
+      @current_module_name = old_module
+    end
+
+    def _def(node)
+      raise ArgumentError, "Children count for def is != 3 (#{node.children.length})" if node.children.length != 3
+
+      puts "def #{node.children[0]}"
+
+      # depending on whether in module or class be clever here ;)
+      @current_class.add_method(node.children[0])
+
+      visit_children(node.children[1..node.children.length])
+    end
+
+    def _class(node)    
+      raise ArgumentError, "Children count for class is != 3 (#{node.children.length})" if node.children.length != 3
       raise ArgumentError, "First class child needs to be a const (#{node.children[0].type} #{node.children[0].type})" if node.children[0].type != :const
 
       puts "clazz #{node.children[0].children[1]}"
 
+      old_class_name = @current_class_name
       old_class = @current_class
-      @current_class = node.children[0].children[1]
-      @references[@current_class] = []
-      visit_children(node)
-      @current_class = node.children[0].children[1]
+
+      @current_class_name = node.children[0].children[1]
+      @current_class = ClassModel.new(@path, @current_module_name, @current_class_name)
+      @classes <<= @current_class
+
+      @references[@current_class_name] = []
+      # recurse over the contents of the class
+      visit_children(node.children[1..node.children.length])
+      @current_class_name = old_class_name
       @current_class = old_class
     end
 
@@ -49,27 +88,30 @@ module Dependencytree
       if node.type() == :const
         const(node)
       elsif node.type() == :class
-        clazz(node)
+        _class(node)
+      elsif node.type() == :module
+        _module(node)
+      elsif node.type() == :def
+        _def(node)
+      else
+        visit_children(node.children)
       end
-      node.children.each { |child|
-        @json_node = {}
+    end
+
+    def visit_children(children)
+      return if ! children
+      children.each { |child|
         if child.respond_to?(:children)
-	  visit_node(child)
+          visit_node(child)
         end
       }
     end
 
-    def visit_children(node)
-      node.children.each { |child|
-        @json_node = {}
-        if child.respond_to?(:children)
-	  visit_node(child)
-        end
-      }
-    end
-
-    def visit()
-      visit_node @tree
+    def visit(path, tree)
+      pp tree
+      @path = path
+      visit_node tree
+      @path = nil
     end
   end
 end
